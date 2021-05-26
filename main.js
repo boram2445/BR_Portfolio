2409ada0a0dd95e15a49211f7dee2994fb984300
boram <boram2445@gmail.com>
2021-05-26 23:31:45 +0900

    JS project filtering

--- main.js
+++ main.js
@@ -50,3 +50,53 @@ const homeSize = home.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
     home.style.opacity = 1-window.scrollY/homeSize;
 })
+
+
+//Show arrow up btn when scrolling down
+const arrow = document.querySelector('.arrow');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > homeSize/2){
+        arrow.classList.add('visible');
+    } else{
+        arrow.classList.remove('visible');
+    }
+})
+
+arrow.addEventListener('click',()=>{
+    scrollIntoView('home');
+})
+
+
+//Project filtering & animation
+const category = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+category.addEventListener('click',(event)=>{
+    const filter = event.target.dataset.filter;
+    if(filter == null){
+        return;
+    } 
+
+    projectContainer.classList.add('anim-out');
+
+    for(let i of category.children){
+        if(i.classList.contains('active')){
+            i.classList.remove('active');
+        }
+    }
+    event.target.classList.add('active');
+    
+    setTimeout(()=>{
+        for(let project of projects){
+            const data = project.dataset.target;
+            if(filter === 'all'){
+                project.classList.remove('invisible');
+            } else if(!(filter === data)){
+                project.classList.add('invisible');
+            } else{
+                project.classList.remove('invisible');
+            }
+        }
+        projectContainer.classList.remove('anim-out');
+    },300);
+})
